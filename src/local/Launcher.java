cf611f0d732721d048a62562ee074d6d5dbad2b2
Yutaka Kato <yutakakato@icloud.com>
2013-10-28 00:41:46 +0900

    renamed package and classes

---
+++ src/local/Launcher.java
@@ -0,0 +1,13 @@
+package local;
+
+
+/** 練習問題起動切換えクラスです。 */
+public class Launcher {
+
+	/** main メソッド */
+	public static void main(String[] args) {
+		// 練習問題を実行
+		local.jpl.chapter1.Problem2.main(null);
+	}
+
+}
---
+++ src/local/jpl/chapter1/Problem1.java
@@ -0,0 +1,8 @@
+package local.jpl.chapter1;
+
+public class Problem1 {
+	
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
