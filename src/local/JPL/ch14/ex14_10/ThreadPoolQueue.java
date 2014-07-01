503ea7ca82427c66e36b06675106a623daff27a6
mikan <yutakakato@icloud.com>
2014-07-01 21:18:19 +0900

    Reviewed ThreadPoolQueue

--- src/local/JPL/ch14/ex14_10/ThreadPoolQueue.java
+++ src/local/JPL/ch14/ex14_10/ThreadPoolQueue.java
@@ -10,16 +10,17 @@ import java.util.LinkedList;
  * 
  * @param <T> Type of element
  */
-@SuppressWarnings("serial")
-public class ThreadPoolQueue<T> extends LinkedList<T> {
+public class ThreadPoolQueue<T> {
 
     private final int size;
+    private LinkedList<T> list;
 
     public ThreadPoolQueue(int size) {
         if (size < 1) {
             throw new IllegalArgumentException("Illegal size of queue.");
         }
         this.size = size;
+        list = new LinkedList<T>();
     }
 
     /**
@@ -28,16 +29,15 @@ public class ThreadPoolQueue<T> extends LinkedList<T> {
      * @param e element to be appended to this list
      * @return true, or false if interrupted on waiting for polling
      */
-    @Override
     public synchronized boolean add(T e) {
-        if (size() >= size)
+        while (list.size() >= size)
             try {
-                wait(); // Wait for poll()
+                wait(); // Wait poll()
             } catch (InterruptedException e1) {
                 return false;
             }
-        boolean result = super.add(e);
-        notifyAll(); // Notify for poll()
+        boolean result = list.add(e);
+        notifyAll(); // Notify poll()
         return result;
     }
 
@@ -46,15 +46,14 @@ public class ThreadPoolQueue<T> extends LinkedList<T> {
      * 
      * @return the head of this list, or null if interrupted on waiting for adding
      */
-    @Override
     public synchronized T poll() {
-        if (isEmpty())
+        if (list.isEmpty())
             try {
-                wait(); // Wait for add()
+                wait(); // Wait add()
             } catch (InterruptedException e1) {
                 return null;
             }
-        T e = super.poll();
+        T e = list.poll();
         notifyAll(); // Notify add()
         return e;
     }
